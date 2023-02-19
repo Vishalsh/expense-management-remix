c5eb289d9a7179d94ed3fdcff46e2418e8c22ef1
Vishal Sharma <vishal.sharma@guccidigital.com>
2023-02-19 16:40:52 +0530

    create a new expense in mongodb cluster using prisma

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -1,6 +1,8 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { addExpense } from "~/data/expenses.server";
 
 export default function AddExpensePage() {
   const navigate = useNavigate();
@@ -14,4 +16,13 @@ export default function AddExpensePage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+  console.log(expenseData);
+  await addExpense(expenseData);
+  
+  return redirect('/expenses');
 }
